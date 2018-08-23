d00b9f37f5f2fde23f7e46cc2b51123f83751c9f
PO-YI CHOU <bob2006tw@gmail.com>
2018-08-23 13:03:50 +0800

    Create arch-install.sh

---
+++ scripts/arch-install.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+
+# Note:
+#     Install archlinux on an "amd64" machine with "UEFI".
+#     Network connection and root privilege are required.
+
+# ================= Partition Table ==================
+# /boot     /dev/sda1 	EFI System Partition    511 M
+# [SWAP]    /dev/sda2 	Linux swap              1.5 G
+# /         /dev/sda3 	Linux                   rest 
+# ====================================================
+
+timedatectl set-ntp true
+
+# Partition
+parted /dev/sda --script \
+    mklabel gpt \
+    mkpart ESP fat32 1MiB 512MiB \
+    set 1 esp on \
+    mkpart primary linux-swap 512MiB 2GiB \
+    mkpart primary ext4 2GiB 100%
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda3
+# initialize partition for swap
+mkswap /dev/sda2
+swapon /dev/sda2
+
+# mount
+mount /dev/sda3 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+
+# select mirrors
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+# extract all the servers of Taiwan
+awk '
+/^## Taiwan$/ { f=1; print $0; next }
+f==0 { next }
+/^$/ { exit }
+{ print $0; f=0 }' /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+# uncomment every mirror
+sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
+# only output the 6 fastest mirrors
+#rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+
+# Install the base packages
+pacstrap /mnt base base-devel
+
+# genfstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# create a script to use afterward
+echo "#!/bin/bash
+
+echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+
+# time zone
+ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime
+hwclock --systohc --utc
+
+echo arch > /etc/hostname
+echo '127.0.0.1	localhost.localdomain	localhost	 arch
+::1	localhost.localdomain	localhost	 arch' >> /etc/hosts
+
+# Initramfs
+pacman -S --noconfirm linux
+mkinitcpio -p linux
+
+# boot loader (grub)
+pacman -S --noconfirm grub efibootmgr
+grub-install /dev/sda --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+# For intel CPU
+pacman -S --noconfirm intel-ucode
+# Generate the main configuration file
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# create /home
+mkdir /home
+echo '/home created'
+
+# Pre-installation
+pacman -Syu --noconfirm --needed bash-completion gcc gdb vim openssh git valgrind dialog wget curl tmux zip unzip
+
+# Change password
+echo 'changing password of root:'
+passwd" > /mnt/archsetup.sh
+# chroot
+arch-chroot /mnt /bin/bash /mnt/archsetup.sh
+rm /mnt/archsetup.sh
+
+umount -R /mnt
+poweroff
+
+# Remember to remove the installation media before booting again
