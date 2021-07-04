d20db1847d022d2e5a9b9e3a90e9ef9e670288b0
Chou, Po-Yi <bob2006tw@gmail.com>
2021-07-04 16:42:59 +0800

    add a simple script to print multiple progress bar

---
+++ scripts/multi_line_progress.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Just a simple test for printing multiple progress bar
+
+function ECHO() {
+    echo $@
+    [ -z "$up" ] && up=1 || up=$((up+1)) # set lines to move up afterwards
+}
+while sleep 1; do
+    [ -n "$up" ] && tput cuu $up && up='' # move up $up lines
+    ECHO "line$((i=i+1))"
+    ECHO "line$((i=i+1))"
+    ECHO "line$((i=i+1))"
+    ECHO "line$((i=i+1))"
+done
